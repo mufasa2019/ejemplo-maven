d689ba192d70ea6b79100c7bc5ae1d9120e382c4
mufasa2019 <cigodoyc@gmail.com>
2022-01-14 22:19:14 -0300

    fix sonar

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.devopsusach2020;
-
+import com.devopsusach2020.model.Pais;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,9 @@ class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+    Pais p = new Pais();
+     assertNotNull(p);
+
 	}
 
 }
